1a83276103e44158ad8868aec7791623aad0d39d
Ed Brown <edbrown@ed-devel.localdomain>
2020-07-28 21:58:52 -0400

    Learning Kafka API and SSL configuration

---
+++ read_topic.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	log.Println("Hi, Ed")
+
+	if len(os.Args) < 4 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <broker> <group> <topics..>\n",
+			os.Args[0])
+		os.Exit(1)
+	}
+
+	broker := os.Args[1]
+	group := os.Args[2]
+	topics := os.Args[3:]
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": broker,
+		// Avoid connecting to IPv6 brokers:
+		// This is needed for the ErrAllBrokersDown show-case below
+		// when using localhost brokers on OSX, since the OSX resolver
+		// will return the IPv6 addresses first.
+		// You typically don't need to specify this configuration property.
+		"broker.address.family":                 "v4",
+		"group.id":                              group,
+		"session.timeout.ms":                    6000,
+		"auto.offset.reset":                     "earliest",
+		"security.protocol":                     "SSL",
+		"ssl.key.location":                      "/home/edbrown/myCA3/client_localhost_client.key",
+		"ssl.key.password":                      "password",
+		"ssl.certificate.location":              "/home/edbrown/myCA3/client_localhost_client.pem",
+		"ssl.ca.location":                       "/home/edbrown/myCA3/ca-cert",
+		"ssl.endpoint.identification.algorithm": "https"})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create consumer: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Created Consumer %v\n", c)
+
+	err = c.SubscribeTopics(topics, nil)
+
+	run := true
+
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			log.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := c.Poll(100)
+			if ev == nil {
+				continue
+			}
+
+			switch e := ev.(type) {
+			case *kafka.Message:
+				log.Printf("%% Message on %s:\n%s\n",
+					e.TopicPartition, string(e.Value))
+				if e.Headers != nil {
+					fmt.Printf("%% Headers: %v\n", e.Headers)
+				}
+			case kafka.Error:
+				// Errors should generally be considered
+				// informational, the client will try to
+				// automatically recover.
+				// But in this example we choose to terminate
+				// the application if all brokers are down.
+				// log.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
+				log.Printf("%% Error: %v: %v\n", e.Code(), e)
+				if e.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			default:
+				log.Printf("Ignored %v\n", e)
+			}
+		}
+	}
+
+	log.Printf("Closing consumer\n")
+	c.Close()
+
+}
